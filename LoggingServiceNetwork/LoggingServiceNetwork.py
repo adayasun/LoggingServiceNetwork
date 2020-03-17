0d7083a0891b1e4a7d2ddcc2b09098e5cf675fe3
Amy Dayasundara <a.dayasundara@gmail.com>
2020-03-17 17:45:50 -0400

    Add project files.

---
+++ LoggingServiceNetwork/LoggingServiceNetwork.py
@@ -0,0 +1,9 @@
+"""
+	FILE			: LoggingServiceNetwork.py
+	PROJECT			: SENG2040 - Assign-03 (A-04)
+	PROGRAMMER		: Amy Dayasundara
+	FIRST VERSION	: 2020-03-17
+	DESCRIPTION		:
+		Service file for logging messages using TCP.
+		Logging information will be store in a text file
+"""
